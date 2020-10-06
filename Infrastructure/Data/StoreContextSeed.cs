814a31d73090796a24d02bfa71f6ec4f4f4f43ab
Devis Bushi <dbushi@outlook.com>
2020-10-06 19:49:46 +0200

    changes section three

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.ProductBrands.Any())
+                {
+                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                }
+            }
+        }
+    }
+}
