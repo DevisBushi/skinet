87b4992e67dfe8fa9669003390305817b38127d8
Devis Bushi <dbushi@outlook.com>
2020-10-06 22:33:53 +0200

    section 3 end

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -30,6 +30,17 @@ namespace API.Controllers
         {
             return await _repo.GetProductByIdAsync(id);
         }
-
+        [HttpGet]
+        [Route("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+        [HttpGet]
+        [Route("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
     }
 }
