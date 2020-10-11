f9e69d6e54aed429a676a9e2fa983f11a432f21b
Devis Bushi <dbushi@outlook.com>
2020-10-11 22:19:56 +0200

    initial ui refactoring

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/model';
-import { IPagination } from './models/pagination';
+import { IProduct } from './shared/models/model';
+import { IPagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
