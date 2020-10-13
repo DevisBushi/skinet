8a48cb8afd86782793f8ac919a10ee04991c5793
Devis Bushi <dbushi@outlook.com>
2020-10-13 16:21:35 +0200

    almost done section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -32,13 +33,21 @@ export class ShopService {
     params = params.append('pageIndex', shopParams.pageNumber.toString());
     params = params.append('pageSize', shopParams.pageSize.toString());
 
-    return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
+    return this.http
+      .get<IPagination>(this.baseUrl + 'products', {
+        observe: 'response',
+        params,
+      })
       .pipe(
-        map(response => {
+        map((response) => {
           return response.body;
         })
       );
   }
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
+  }
+
   getBrands() {
     return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
   }
