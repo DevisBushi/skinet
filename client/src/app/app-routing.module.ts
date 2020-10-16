c411569a378394c29618a5ccc7ddead78dca3586
Devis Bushi <dbushi@outlook.com>
2020-10-16 16:07:41 +0200

    end of day section 14

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -27,6 +27,11 @@ const routes: Routes = [
       import('./shop/shop.module').then((mod) => mod.ShopModule),
     data: { breadcrumb: 'Shop' },
   },
+{  path: 'basket',
+    loadChildren: () =>
+      import('./basket/basket.module').then((mod) => mod.BasketModule),
+    data: { breadcrumb: 'basket' },
+  },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
